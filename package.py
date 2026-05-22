bd796dd54aae71ca6cd779505ce3f2326e93b33e
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-05-22 19:05:09 +0200

    added service to services

--- package.py
+++ package.py
@@ -2,6 +2,10 @@ name = "hibob"
 title = "HiBob"
 version = "1.0.2-dev"
 
+services = {
+    "ayon_sync": {"image": f"ynput/ayon-hibob-leecher:1.0.0"},
+}
+
 ayon_required_addons = {}
 ayon_compatible_addons = {
     "ftrack": ">1.1.4",
