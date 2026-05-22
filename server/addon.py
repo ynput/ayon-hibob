3131a581345a291912399dbe75e52a2a05267ffb
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-05-22 19:20:01 +0200

    actually use conversion

--- server/addon.py
+++ server/addon.py
@@ -1,8 +1,12 @@
-from typing import Type
+from typing import Type, Any
 
 from ayon_server.addons import BaseServerAddon
 
-from .settings import HiBobSettings, DEFAULT_VALUES
+from .settings import (
+    HiBobSettings,
+    DEFAULT_VALUES,
+    convert_settings_overrides,
+)
 
 
 class HiBobAddon(BaseServerAddon):
@@ -11,6 +15,16 @@ class HiBobAddon(BaseServerAddon):
     async def get_default_settings(self):
         settings_model_cls = self.get_settings_model()
         return settings_model_cls(**DEFAULT_VALUES)
+
+    async def convert_settings_overrides(
+        self,
+        source_version: str,
+        overrides: dict[str, Any],
+    ) -> dict[str, Any]:
+        await convert_settings_overrides(source_version, overrides)
+        return await super().convert_settings_overrides(
+            source_version, overrides
+        )
 
     def get_custom_ftrack_handlers_endpoint(self) -> str:
         return (
