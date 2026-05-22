dab1fb5a64c9541835aaa929a2fece2f1d715dc3
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-05-22 19:05:01 +0200

    change compatibility checks

--- package.py
+++ package.py
@@ -2,7 +2,8 @@ name = "hibob"
 title = "HiBob"
 version = "1.0.2-dev"
 
-ayon_required_addons = {
-    "ftrack": ">1.1.4"
+ayon_required_addons = {}
+ayon_compatible_addons = {
+    "ftrack": ">1.1.4",
+    "planner": ">1.0.0",
 }
-ayon_compatible_addons = {}
